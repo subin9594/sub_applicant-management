eefb107b06b5394e964d7d277874b3f4ba7e5abd
조수빈 <subo3os2@gmail.com>
2025-07-22 21:35:34 +0900

    서버 오류 고치는 중ing...

--- src/main/java/com/kuhas/applicant_managementt/config/SecurityConfig.java
+++ src/main/java/com/kuhas/applicant_managementt/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://10.0.2.2:8080", "http://localhost:8080", "http://localhost:53608") // 실제 앱/웹 주소로 수정
+            .allowedOriginPatterns("*") // allowCredentials와 함께 사용 가능
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
